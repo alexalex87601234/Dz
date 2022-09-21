59d50ad472c9b9296211bc9da53108b68e3199d1
Alex <alexalexalex876012345@gmail.com>
2022-09-21 14:45:01 +0300

    Variant_2

--- Dz_1/script.js
+++ Dz_1/script.js
@@ -23,6 +23,28 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
+/* Variant_1 */
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+// };
+
+// const lastMovie = prompt('Один из последних просмотренных фильмов?', '');
+// const ratingMovie = prompt('На сколько оцените его?', '');
+
+// personalMovieDB.movies.lastMovie;
+// personalMovieDB.movies.lastMovie = ratingMovie;
+
+// console.log(personalMovieDB)
+
+/* Variant_2 */
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
@@ -33,13 +55,12 @@ const personalMovieDB = {
     privat: false,
 };
 
-const lastMovie = prompt('Один из последних просмотренных фильмов?', '');
-const ratingMovie = prompt('На сколько оцените его?', '');
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = +prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies.lastMovie;
-personalMovieDB.movies.lastMovie = ratingMovie;
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB)
-
-
-
+console.log(personalMovieDB);
